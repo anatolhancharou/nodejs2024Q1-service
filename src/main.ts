5cf8a3467bb31160013f9980be460633d12d607c
Anatol Hancharou <a.potteroff@gmail.com>
2024-03-10 16:25:11 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
+import { readFile } from 'node:fs/promises';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const file = await readFile('./doc/api.yaml', { encoding: 'utf8' });
+  const swaggerDocument = parse(file);
+
+  SwaggerModule.setup('doc', app, swaggerDocument);
 
   const port = process.env.PORT || 4000;
 
